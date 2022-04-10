60a5cc72570c68a45bea909c75ca4c56082905fd
jihoon2020 <83260875+jihoon2020@users.noreply.github.com>
2022-04-10 15:35:29 +0900

    Initial commit

---
+++ src/main/java/com/lunar/lunarMgmt/LunarMgmtApplication.java
@@ -0,0 +1,13 @@
+package com.lunar.lunarMgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunarMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunarMgmtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lunar/lunarMgmt/LunarMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lunar.lunarMgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LunarMgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
