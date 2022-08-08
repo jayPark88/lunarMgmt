4a25f94e92621982cf78e8adae85bdcb618ef35b
jayPark <jaypark2828@gmail.com>
2022-08-08 12:54:58 +0900

    ⭐modify : junit QueryDslTest 추가

--- src/test/java/com/lunar/lunarMgmt/api/QueryDslTest.java
+++ src/test/java/com/lunar/lunarMgmt/api/QueryDslTest.java
@@ -2,12 +2,21 @@ package com.lunar.lunarMgmt.api;
 
 import com.lunar.lunarMgmt.LunarMgmtApplication;
 import com.lunar.lunarMgmt.api.login.model.AdminUserDto;
+import com.lunar.lunarMgmt.common.jpa.entities.AdminUserEntity;
+import static com.lunar.lunarMgmt.common.jpa.entities.QAdminUserEntity.adminUserEntity;
 import com.lunar.lunarMgmt.common.jpa.repository.AdminUserRepository;
+import com.querydsl.jpa.impl.JPAQuery;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.assertj.core.api.Assertions;
+
+import javax.persistence.EntityManager;
+import java.util.List;
 
 @SpringBootTest(classes = LunarMgmtApplication.class)
 @DisplayName("Admin User의 이름이 p를 포함한 데이터를 조회한다.")
@@ -15,6 +24,8 @@ class QueryDslTest {
 
     @Autowired
     private AdminUserRepository adminUserRepository;
+    @Autowired
+    private EntityManager em;
 
     @Test
     public void searchTest(){
@@ -26,5 +37,18 @@ class QueryDslTest {
 
         // then
         Assertions.assertThat(adminUserDto.getAdminUserId()).isEqualTo(adminUserRepository.findByAdminUserId(adminUserId).getAdminUserId());
+    }
+
+    @Test
+    public void queryDslTest(){
+        //given
+        String adminUserId = "jayPark";
+
+        //when
+        JPAQuery<AdminUserEntity> query  = new JPAQuery<>(em);
+
+        List<AdminUserEntity> adminUserList = query.from(adminUserEntity)
+                .where(adminUserEntity.adminUserId.contains("Park")).fetch();
+        Assertions.assertThat(adminUserList).hasSize(2);
     }
 }
