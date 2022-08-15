887c52039c60dcb6cf2ba2e50b2a27082cbdf56c
jayPark <jaypark8282@gmail.com>
2022-08-16 00:26:03 +0900

    ⭐modify: HQ-ADMIN appConfig 관련 분석 주석 추가
    ⭐modify: HQ-ADMIN appConfig 관련 분석 주석 추가

--- src/main/java/com/lunar/lunarMgmt/common/config/AppConfig.java
+++ src/main/java/com/lunar/lunarMgmt/common/config/AppConfig.java
@@ -8,4 +8,5 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
     // 주로 복잡한 추상 클래스에서 사용하기 보다는
     // 단순한 interface기반으로 구현체를 주입을 할때 사용을 하면 좋을 것 같다.
+    // appconfig에 생성자 주입을 하면 꼬이는 것 같다.
 }
