47a9acea24fc6bb83a8707bfb092542ed69e24e3
jayPark <jaypark8282@gmail.com>
2022-10-05 22:57:10 +0900

    Code clean

--- src/main/java/com/lunar/lunarMgmt/api/community/notice/controller/NoticeController.java
+++ src/main/java/com/lunar/lunarMgmt/api/community/notice/controller/NoticeController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/notice")
 public class NoticeController {
+
     @PostMapping
     public void saveNoticeInfo(){
 
