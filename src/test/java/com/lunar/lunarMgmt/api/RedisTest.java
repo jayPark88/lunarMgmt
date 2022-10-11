a8f3eb39034f356fcc02f4e2cfada884c6ac8d1f
jayPark <jaypark8282@gmail.com>
2022-10-11 22:34:24 +0900

    1 day 1 commit
    1 day 1 commit

--- src/test/java/com/lunar/lunarMgmt/api/RedisTest.java
+++ src/test/java/com/lunar/lunarMgmt/api/RedisTest.java
@@ -11,6 +11,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(classes = LunarMgmtApplication.class)
 public class RedisTest {
+
     @Autowired
     private RedisRepositoryConfig redisRepositoryConfig;
 
