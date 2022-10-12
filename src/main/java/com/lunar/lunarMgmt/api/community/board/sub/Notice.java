ba8dd1817c946eae9e6c571dd2ba576cae551fe5
jayPark <jaypark2828@gmail.com>
2022-10-12 14:57:54 +0900

    🎆add: Notice 자식 클래스

---
+++ src/main/java/com/lunar/lunarMgmt/api/community/board/sub/Notice.java
@@ -0,0 +1,17 @@
+package com.lunar.lunarMgmt.api.community.board.sub;
+
+import com.lunar.lunarMgmt.api.community.board.abst.BoardAbstract;
+import com.lunar.lunarMgmt.api.community.board.model.BoardDto;
+import com.lunar.lunarMgmt.common.jpa.repository.BoardRepository;
+
+public class Notice extends BoardAbstract {
+
+    public Notice(BoardRepository boardRepository) {
+        super(boardRepository);
+    }
+
+    @Override
+    public void boardInfoSave(BoardDto boardDto) {
+        boardRepository.save(boardDto.to());
+    }
+}
