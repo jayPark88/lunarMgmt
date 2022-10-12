d593028f721efba6787b4e4cddc31c66416c4bd1
jayPark <jaypark2828@gmail.com>
2022-10-12 14:49:34 +0900

    ⭐add : AdminBoardTest 테스트 코드 추가

---
+++ src/test/java/com/lunar/lunarMgmt/api/AdminBoardTest.java
@@ -0,0 +1,17 @@
+package com.lunar.lunarMgmt.api;
+
+import com.lunar.lunarMgmt.LunarMgmtApplication;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest(classes = LunarMgmtApplication.class)
+public class AdminBoardTest {
+
+    @Test
+    @DisplayName("notice create Test")
+    public void noticeCreateTest(){
+
+    }
+}
