8b57cac9b50c8462351cca4106311116dd7d8880
jayPark <jaypark8282@gmail.com>
2022-10-14 17:38:20 +0900

    🐖modify: notice Controller save 수정
    🐖modify: notice Controller save 수정

--- src/main/java/com/lunar/lunarMgmt/api/community/board/controller/NoticeController.java
+++ src/main/java/com/lunar/lunarMgmt/api/community/board/controller/NoticeController.java
@@ -1,12 +1,16 @@
 package com.lunar.lunarMgmt.api.community.board.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lunar.lunarMgmt.api.community.board.model.BoardDto;
 import com.lunar.lunarMgmt.api.community.board.service.NoticeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.SneakyThrows;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.UnsupportedEncodingException;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,8 +19,13 @@ public class NoticeController {
 
     private final NoticeService noticeService;
 
-    @PostMapping
-    public void saveNoticeInfo(@RequestBody BoardDto boardDto){
+    @SneakyThrows
+    @PostMapping(consumes = { MediaType.MULTIPART_FORM_DATA_VALUE })
+    public void saveNoticeInfo(@RequestPart("boardDto") String boardString,
+                               @RequestPart(value="files", required = false)List<MultipartFile> files){
+        ObjectMapper om = new ObjectMapper();
+        BoardDto boardDto = om.readValue(new String(boardString.getBytes("8859_1"),"UTF-8"), BoardDto.class);
+
         noticeService.saveNotice(boardDto);
     }
 }
