5e7a25dbbf91e95e0630c405cf865fe501d88d4f
jayPark <jaypark8282@gmail.com>
2022-10-25 13:47:31 +0900

    🐖modify: saveNotice controller files 추가
    🐖modify: saveNotice controller files 추가

--- src/main/java/com/lunar/lunarMgmt/api/community/board/service/NoticeService.java
+++ src/main/java/com/lunar/lunarMgmt/api/community/board/service/NoticeService.java
@@ -4,13 +4,17 @@ import com.lunar.lunarMgmt.api.community.board.model.BoardDto;
 import com.lunar.lunarMgmt.api.community.board.sub.Notice;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class NoticeService {
     private final Notice notice;
 
-    public void saveNotice(BoardDto boardDto){
+    public void saveNotice(BoardDto boardDto, List<MultipartFile> files){
+        boardDto.setFiles(notice.createFileDtoList(files));
         notice.boardInfoSave(boardDto);
     }
 }
