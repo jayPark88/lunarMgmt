7e3ed3ddf84d8d4c132f7dbffd133195cf7df44a
jayPark <jaypark8282@gmail.com>
2022-10-25 13:22:00 +0900

    🐖modify: 부모 클래스 생성자 수정
    🐖modify: 부모 클래스 생성자 수정

--- src/main/java/com/lunar/lunarMgmt/api/community/board/sub/Notice.java
+++ src/main/java/com/lunar/lunarMgmt/api/community/board/sub/Notice.java
@@ -2,14 +2,16 @@ package com.lunar.lunarMgmt.api.community.board.sub;
 
 import com.lunar.lunarMgmt.api.community.board.abst.BoardAbstract;
 import com.lunar.lunarMgmt.api.community.board.model.BoardDto;
+import com.lunar.lunarMgmt.common.config.yml.FileConfig;
+import com.lunar.lunarMgmt.common.intf.FileUtil;
 import com.lunar.lunarMgmt.common.jpa.repository.BoardRepository;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Notice extends BoardAbstract {
 
-    public Notice(BoardRepository boardRepository) {
-        super(boardRepository);
+    public Notice(FileConfig fileConfig, BoardRepository boardRepository, FileUtil fileUtil) {
+        super(fileConfig, boardRepository, fileUtil);
     }
 
     @Override
