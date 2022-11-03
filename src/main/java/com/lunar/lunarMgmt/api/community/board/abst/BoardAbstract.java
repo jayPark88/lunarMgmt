2215f3629dda3c24fb3f1791508ab163afbbdcaf
jayPark <jaypark8282@gmail.com>
2022-11-03 16:32:42 +0900

    🐿add: notice 목록 조회 기능 프로토타입 추가 (테스트 필요)
    🐿add: notice 목록 조회 기능 프로토타입 추가 (테스트 필요)

--- src/main/java/com/lunar/lunarMgmt/api/community/board/abst/BoardAbstract.java
+++ src/main/java/com/lunar/lunarMgmt/api/community/board/abst/BoardAbstract.java
@@ -1,10 +1,14 @@
 package com.lunar.lunarMgmt.api.community.board.abst;
 
 import com.lunar.lunarMgmt.api.community.board.model.BoardDto;
+import com.lunar.lunarMgmt.api.community.board.model.BoardSearchDto;
 import com.lunar.lunarMgmt.common.config.yml.FileConfig;
 import com.lunar.lunarMgmt.common.intf.FileUtil;
+import com.lunar.lunarMgmt.common.jpa.entities.BoardEntity;
 import com.lunar.lunarMgmt.common.jpa.repository.BoardRepository;
 import com.lunar.lunarMgmt.common.model.FileDto;
+import com.lunar.lunarMgmt.common.model.PageRequest;
+import com.lunar.lunarMgmt.common.model.PageResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.util.ObjectUtils;
@@ -24,6 +28,8 @@ public abstract class BoardAbstract {
 
     // board 정보 save method
     protected abstract void boardInfoSave(BoardDto boardDto);
+    // board 목록 조회
+    protected abstract PageResponse<BoardEntity,BoardDto> selectBoardList(BoardSearchDto boardSearchDto, PageRequest pageRequest);
 
     // fileDto생성 method
     public List<FileDto> createFileDtoList(List<MultipartFile> files) throws IOException {
--- src/main/java/com/lunar/lunarMgmt/api/community/board/sub/Notice.java
+++ src/main/java/com/lunar/lunarMgmt/api/community/board/sub/Notice.java
@@ -2,12 +2,19 @@ package com.lunar.lunarMgmt.api.community.board.sub;
 
 import com.lunar.lunarMgmt.api.community.board.abst.BoardAbstract;
 import com.lunar.lunarMgmt.api.community.board.model.BoardDto;
+import com.lunar.lunarMgmt.api.community.board.model.BoardSearchDto;
 import com.lunar.lunarMgmt.common.config.yml.FileConfig;
 import com.lunar.lunarMgmt.common.intf.FileUtil;
+import com.lunar.lunarMgmt.common.jpa.entities.BoardEntity;
 import com.lunar.lunarMgmt.common.jpa.repository.BoardRepository;
+import com.lunar.lunarMgmt.common.model.PageRequest;
+import com.lunar.lunarMgmt.common.model.PageResponse;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
+@Transactional
 public class Notice extends BoardAbstract {
 
     public Notice(FileConfig fileConfig, BoardRepository boardRepository, FileUtil fileUtil) {
@@ -17,5 +24,14 @@ public class Notice extends BoardAbstract {
     @Override
     public void boardInfoSave(BoardDto boardDto) {
         boardRepository.save(boardDto.to());
+    }
+
+    @Override
+    protected PageResponse<BoardEntity, BoardDto> selectBoardList(BoardSearchDto boardSearchDto, PageRequest pageRequest) {
+        pageRequest.setSort("boardSeq");
+        pageRequest.setDirection("desc");
+
+        Page<BoardDto> list = boardRepository.searchBoardList(boardSearchDto, pageRequest);
+        return null;
     }
 }
