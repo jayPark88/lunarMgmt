f558c2e25be62a928691550df24dd5d53f1eaa16
jayPark <jaypark8282@gmail.com>
2022-11-04 18:25:50 +0900

    🐖add: boardSearchDto 추가
    🐖add: boardSearchDto 추가

--- src/main/java/com/lunar/lunarMgmt/api/community/board/model/BoardSearchDto.java
+++ src/main/java/com/lunar/lunarMgmt/api/community/board/model/BoardSearchDto.java
@@ -1,9 +1,11 @@
 package com.lunar.lunarMgmt.api.community.board.model;
 
 import com.lunar.lunarMgmt.common.model.enums.BoardTypeEnum;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class BoardSearchDto {
     private BoardTypeEnum boardTypeEnum;
     private String title;
