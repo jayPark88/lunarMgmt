21e2ff92d5867e085bd90f3669a4cbc0afc13acf
jayPark <jaypark8282@gmail.com>
2022-11-06 15:05:59 +0900

    🐖add: 공지사항 controller API, service 추가
    🐖add: 공지사항 controller API, service 추가

--- src/main/java/com/lunar/lunarMgmt/api/community/board/controller/NoticeController.java
+++ src/main/java/com/lunar/lunarMgmt/api/community/board/controller/NoticeController.java
@@ -2,7 +2,11 @@ package com.lunar.lunarMgmt.api.community.board.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lunar.lunarMgmt.api.community.board.model.BoardDto;
+import com.lunar.lunarMgmt.api.community.board.model.BoardSearchDto;
 import com.lunar.lunarMgmt.api.community.board.service.NoticeService;
+import com.lunar.lunarMgmt.common.jpa.entities.BoardEntity;
+import com.lunar.lunarMgmt.common.model.PageRequest;
+import com.lunar.lunarMgmt.common.model.PageResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.http.MediaType;
@@ -25,5 +29,10 @@ public class NoticeController {
         ObjectMapper om = new ObjectMapper();
         BoardDto boardDto = om.readValue(new String(boardString.getBytes("8859_1"),"UTF-8"), BoardDto.class);
         noticeService.saveNotice(boardDto, files);
+    }
+
+    @GetMapping("/list")
+    public PageResponse<BoardEntity, BoardDto> getList(@ModelAttribute BoardSearchDto boardSearchDto, @ModelAttribute PageRequest pageRequest){
+        return noticeService.selectBoardList(boardSearchDto, pageRequest);
     }
 }
--- src/main/java/com/lunar/lunarMgmt/api/community/board/service/NoticeService.java
+++ src/main/java/com/lunar/lunarMgmt/api/community/board/service/NoticeService.java
@@ -1,7 +1,11 @@
 package com.lunar.lunarMgmt.api.community.board.service;
 
 import com.lunar.lunarMgmt.api.community.board.model.BoardDto;
+import com.lunar.lunarMgmt.api.community.board.model.BoardSearchDto;
 import com.lunar.lunarMgmt.api.community.board.sub.Notice;
+import com.lunar.lunarMgmt.common.jpa.entities.BoardEntity;
+import com.lunar.lunarMgmt.common.model.PageRequest;
+import com.lunar.lunarMgmt.common.model.PageResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -17,5 +21,9 @@ public class NoticeService {
     public void saveNotice(BoardDto boardDto, List<MultipartFile> files) throws IOException {
         boardDto.setFiles(notice.createFileDtoList(files));
         notice.boardInfoSave(boardDto);
+    }
+
+    public PageResponse<BoardEntity, BoardDto> selectBoardList(BoardSearchDto boardSearchDto, PageRequest pageRequest){
+        return notice.selectBoardList(boardSearchDto, pageRequest);
     }
 }
